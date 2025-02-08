69ef6981b0f6a35fac8e5a68dd8e46e7865bbf1d
ffelixg <142172984+ffelixg@users.noreply.github.com>
2025-02-09 00:38:24 +0100

    Expose mem

--- src/odbc/root.zig
+++ src/odbc/root.zig
@@ -3,3 +3,4 @@ pub const attributes = @import("attributes.zig");
 pub const info = @import("info.zig");
 pub const return_codes = @import("return_codes.zig");
 pub const sql = @import("sql.zig");
+pub const mem = @import("mem.zig");
