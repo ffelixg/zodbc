be23fa4687865284d64b713a00a17d77cee1febe
ffelixg <142172984+ffelixg@users.noreply.github.com>
2025-07-13 02:17:21 +0200

    String Descriptor fields

--- src/core/Descriptor.zig
+++ src/core/Descriptor.zig
@@ -26,10 +26,7 @@ fn FieldValueUnion(comptime descriptor_kind: DescriptorKind) type {
         count: i16,
         rows_processed_ptr: ?*u64,
         auto_unique_value: bool,
-        base_column_name: ?[*:0]u8,
-        base_table_name: ?[*:0]u8,
         case_sensitive: bool,
-        catalog_name: ?[*:0]u8,
         concise_type: switch (descriptor_kind) {
             .app_param_desc, .app_row_desc => types.CDataType,
             .imp_param_desc, .imp_row_desc => types.SQLDataType,
@@ -40,12 +37,7 @@ fn FieldValueUnion(comptime descriptor_kind: DescriptorKind) type {
         display_size: i64,
         fixed_prec_scale: bool,
         indicator_ptr: ?[*]i64,
-        label: ?[*:0]u8,
         length: u64,
-        literal_prefix: ?[*:0]u8,
-        literal_suffix: ?[*:0]u8,
-        local_type_name: ?[*:0]u8,
-        name: ?[*:0]u8,
         nullable: attrs.Nullable,
         num_prec_radix: attrs.NumPrecRadix,
         octet_length: i64,
@@ -54,14 +46,11 @@ fn FieldValueUnion(comptime descriptor_kind: DescriptorKind) type {
         precision: i16,
         rowver: bool,
         scale: i16,
-        schema_name: ?[*:0]u8,
         searchable: attrs.Searchable,
-        table_name: ?[*:0]u8,
         type: switch (descriptor_kind) {
             .app_param_desc, .app_row_desc => types.CDataType,
             .imp_param_desc, .imp_row_desc => types.SQLDataType,
         },
-        type_name: ?[*:0]u8,
         unnamed: attrs.Unnamed,
         unsigned: bool,
         updatable: attrs.Updatable,
@@ -83,12 +72,12 @@ fn FieldType(comptime attr: anytype, comptime descriptor_kind: DescriptorKind) t
 
 fn getFieldGeneric(
     handle: ?*anyopaque,
-    col_number: i16,
+    col_number: u15,
     comptime attr: anytype,
     comptime descriptor_kind: DescriptorKind,
 ) !FieldType(attr, descriptor_kind) {
     var value_ptr: ?*anyopaque = null;
-    switch (c.SQLGetDescField(
+    switch (c.SQLGetDescFieldW(
         handle,
         col_number,
         @intFromEnum(attr),
@@ -108,7 +97,7 @@ fn getFieldGeneric(
 
 fn setFieldGeneric(
     handle: ?*anyopaque,
-    col_number: i16,
+    col_number: u15,
     comptime attr: anytype,
     comptime descriptor_kind: DescriptorKind,
     value: FieldType(attr, descriptor_kind),
@@ -119,6 +108,58 @@ fn setFieldGeneric(
         @intFromEnum(attr),
         @ptrFromInt(@import("utils.zig").toUsize(value)),
         0,
+    )) {
+        c.SQL_SUCCESS => {},
+        c.SQL_SUCCESS_WITH_INFO => error.SetDescFieldSuccessWithInfo,
+        c.SQL_ERROR => error.SetDescFieldError,
+        c.SQL_INVALID_HANDLE => error.SetDescFieldInvalidHandle,
+        else => unreachable,
+    };
+}
+
+fn getFieldGenericString(
+    handle: ?*anyopaque,
+    col_number: u15,
+    comptime attr: anytype,
+    allocator: std.mem.Allocator,
+) ![:0]u8 {
+    var buffer: [1024]u16 = undefined;
+    var len: isize = 0;
+    switch (c.SQLGetDescFieldW(
+        handle,
+        col_number,
+        @intFromEnum(attr),
+        @ptrCast(&buffer),
+        buffer.len,
+        @ptrCast(&len),
+    )) {
+        c.SQL_SUCCESS => {},
+        c.SQL_SUCCESS_WITH_INFO => return error.GetDescFieldSuccessWithInfo,
+        c.SQL_ERROR => return error.GetDescFieldError,
+        c.SQL_NO_DATA => return error.GetDescFieldNoData,
+        c.SQL_INVALID_HANDLE => return error.GetDescFieldInvalidHandle,
+        else => unreachable,
+    }
+    return try std.unicode.wtf16LeToWtf8AllocZ(
+        allocator,
+        buffer[0..@intCast(@divExact(len, 2))],
+    );
+}
+
+fn setFieldGenericString(
+    handle: ?*anyopaque,
+    col_number: u15,
+    comptime attr: anytype,
+    value: []const u8,
+) !void {
+    const value_wide = try std.unicode.wtf8ToWtf16LeAlloc(std.heap.smp_allocator, value);
+    defer std.heap.smp_allocator.free(value_wide);
+    return switch (c.SQLSetDescFieldW(
+        handle,
+        col_number,
+        @intFromEnum(attr),
+        @ptrCast(value_wide.ptr),
+        @intCast(value_wide.len * 2),
     )) {
         c.SQL_SUCCESS => {},
         c.SQL_SUCCESS_WITH_INFO => error.SetDescFieldSuccessWithInfo,
@@ -179,35 +220,38 @@ const ReadFieldsImpRowDesc = enum(u15) {
     alloc_type = c.SQL_DESC_ALLOC_TYPE,
     count = c.SQL_DESC_COUNT,
     auto_unique_value = c.SQL_DESC_AUTO_UNIQUE_VALUE,
-    base_column_name = c.SQL_DESC_BASE_COLUMN_NAME,
-    base_table_name = c.SQL_DESC_BASE_TABLE_NAME,
     case_sensitive = c.SQL_DESC_CASE_SENSITIVE,
-    catalog_name = c.SQL_DESC_CATALOG_NAME,
     concise_type = c.SQL_DESC_CONCISE_TYPE,
     datetime_interval_code = c.SQL_DESC_DATETIME_INTERVAL_CODE,
     datetime_interval_precision = c.SQL_DESC_DATETIME_INTERVAL_PRECISION,
     display_size = c.SQL_DESC_DISPLAY_SIZE,
     fixed_prec_scale = c.SQL_DESC_FIXED_PREC_SCALE,
-    label = c.SQL_DESC_LABEL,
     length = c.SQL_DESC_LENGTH,
-    literal_prefix = c.SQL_DESC_LITERAL_PREFIX,
-    literal_suffix = c.SQL_DESC_LITERAL_SUFFIX,
-    local_type_name = c.SQL_DESC_LOCAL_TYPE_NAME,
-    name = c.SQL_DESC_NAME,
     nullable = c.SQL_DESC_NULLABLE,
     num_prec_radix = c.SQL_DESC_NUM_PREC_RADIX,
     octet_length = c.SQL_DESC_OCTET_LENGTH,
     precision = c.SQL_DESC_PRECISION,
     rowver = c.SQL_DESC_ROWVER,
     scale = c.SQL_DESC_SCALE,
-    schema_name = c.SQL_DESC_SCHEMA_NAME,
     searchable = c.SQL_DESC_SEARCHABLE,
-    table_name = c.SQL_DESC_TABLE_NAME,
     type = c.SQL_DESC_TYPE,
-    type_name = c.SQL_DESC_TYPE_NAME,
     unnamed = c.SQL_DESC_UNNAMED,
     unsigned = c.SQL_DESC_UNSIGNED,
     updatable = c.SQL_DESC_UPDATABLE,
+};
+
+const ReadFieldsImpRowDescString = enum(u15) {
+    base_column_name = c.SQL_DESC_BASE_COLUMN_NAME,
+    base_table_name = c.SQL_DESC_BASE_TABLE_NAME,
+    catalog_name = c.SQL_DESC_CATALOG_NAME,
+    label = c.SQL_DESC_LABEL,
+    literal_prefix = c.SQL_DESC_LITERAL_PREFIX,
+    literal_suffix = c.SQL_DESC_LITERAL_SUFFIX,
+    schema_name = c.SQL_DESC_SCHEMA_NAME,
+    table_name = c.SQL_DESC_TABLE_NAME,
+    type_name = c.SQL_DESC_TYPE_NAME,
+    local_type_name = c.SQL_DESC_LOCAL_TYPE_NAME,
+    name = c.SQL_DESC_NAME,
 };
 
 const ReadFieldsImpParamDesc = enum(u15) {
@@ -218,7 +262,6 @@ const ReadFieldsImpParamDesc = enum(u15) {
     datetime_interval_code = c.SQL_DESC_DATETIME_INTERVAL_CODE,
     datetime_interval_precision = c.SQL_DESC_DATETIME_INTERVAL_PRECISION,
     length = c.SQL_DESC_LENGTH,
-    name = c.SQL_DESC_NAME,
     num_prec_radix = c.SQL_DESC_NUM_PREC_RADIX,
     octet_length = c.SQL_DESC_OCTET_LENGTH,
     parameter_type = c.SQL_DESC_PARAMETER_TYPE,
@@ -230,11 +273,15 @@ const ReadFieldsImpParamDesc = enum(u15) {
     alloc_type = c.SQL_DESC_ALLOC_TYPE,
     case_sensitive = c.SQL_DESC_CASE_SENSITIVE,
     fixed_prec_scale = c.SQL_DESC_FIXED_PREC_SCALE,
-    local_type_name = c.SQL_DESC_LOCAL_TYPE_NAME,
     nullable = c.SQL_DESC_NULLABLE,
     rowver = c.SQL_DESC_ROWVER,
     type_name = c.SQL_DESC_TYPE_NAME,
     unsigned = c.SQL_DESC_UNSIGNED,
+};
+
+const ReadFieldsImpParamDescString = enum(u15) {
+    local_type_name = c.SQL_DESC_LOCAL_TYPE_NAME,
+    name = c.SQL_DESC_NAME,
 };
 
 const WriteFieldsAppRowDesc = enum(u15) {
@@ -290,7 +337,6 @@ const WriteFieldsImpParamDesc = enum(u15) {
     datetime_interval_code = c.SQL_DESC_DATETIME_INTERVAL_CODE,
     datetime_interval_precision = c.SQL_DESC_DATETIME_INTERVAL_PRECISION,
     length = c.SQL_DESC_LENGTH,
-    name = c.SQL_DESC_NAME,
     num_prec_radix = c.SQL_DESC_NUM_PREC_RADIX,
     octet_length = c.SQL_DESC_OCTET_LENGTH,
     parameter_type = c.SQL_DESC_PARAMETER_TYPE,
@@ -298,6 +344,10 @@ const WriteFieldsImpParamDesc = enum(u15) {
     scale = c.SQL_DESC_SCALE,
     type = c.SQL_DESC_TYPE,
     unnamed = c.SQL_DESC_UNNAMED,
+};
+
+const WriteFieldsImpParamDescString = enum(u15) {
+    name = c.SQL_DESC_NAME,
 };
 
 pub const AppRowDesc = struct {
@@ -323,7 +373,7 @@ pub const AppRowDesc = struct {
 
     pub fn setField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime field: WriteFieldsAppRowDesc,
         value: FieldType(field, .app_row_desc),
     ) !void {
@@ -338,7 +388,7 @@ pub const AppRowDesc = struct {
 
     pub fn getField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime attr: ReadFieldsAppRowDesc,
     ) !FieldType(attr, .app_row_desc) {
         return try getFieldGeneric(
@@ -373,7 +423,7 @@ pub const ImpRowDesc = struct {
 
     pub fn setField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime field: WriteFieldsImpRowDesc,
         value: FieldType(field, .imp_row_desc),
     ) !void {
@@ -388,7 +438,7 @@ pub const ImpRowDesc = struct {
 
     pub fn getField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime attr: ReadFieldsImpRowDesc,
     ) !FieldType(attr, .imp_row_desc) {
         return try getFieldGeneric(
@@ -396,6 +446,20 @@ pub const ImpRowDesc = struct {
             col_number,
             attr,
             .imp_row_desc,
+        );
+    }
+
+    pub fn getFieldString(
+        self: Self,
+        col_number: u15,
+        comptime attr: ReadFieldsImpRowDescString,
+        allocator: std.mem.Allocator,
+    ) ![:0]u8 {
+        return try getFieldGenericString(
+            self.handle(),
+            col_number,
+            attr,
+            allocator,
         );
     }
 };
@@ -423,7 +487,7 @@ pub const AppParamDesc = struct {
 
     pub fn setField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime field: WriteFieldsAppParamDesc,
         value: FieldType(field, .app_param_desc),
     ) !void {
@@ -438,7 +502,7 @@ pub const AppParamDesc = struct {
 
     pub fn getField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime attr: ReadFieldsAppParamDesc,
     ) !FieldType(attr, .app_param_desc) {
         return try getFieldGeneric(
@@ -473,7 +537,7 @@ pub const ImpParamDesc = struct {
 
     pub fn setField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime field: WriteFieldsImpParamDesc,
         value: FieldType(field, .imp_param_desc),
     ) !void {
@@ -486,9 +550,23 @@ pub const ImpParamDesc = struct {
         );
     }
 
+    pub fn setFieldString(
+        self: Self,
+        col_number: u15,
+        comptime field: WriteFieldsImpParamDescString,
+        value: []const u8,
+    ) !void {
+        try setFieldGenericString(
+            self.handle(),
+            col_number,
+            field,
+            value,
+        );
+    }
+
     pub fn getField(
         self: Self,
-        col_number: i16,
+        col_number: u15,
         comptime attr: ReadFieldsImpParamDesc,
     ) !FieldType(attr, .imp_param_desc) {
         return try getFieldGeneric(
@@ -496,6 +574,20 @@ pub const ImpParamDesc = struct {
             col_number,
             attr,
             .imp_param_desc,
+        );
+    }
+
+    pub fn getFieldString(
+        self: Self,
+        col_number: u15,
+        comptime attr: ReadFieldsImpParamDescString,
+        allocator: std.mem.Allocator,
+    ) ![:0]u8 {
+        return try getFieldGenericString(
+            self.handle(),
+            col_number,
+            attr,
+            allocator,
         );
     }
 };
